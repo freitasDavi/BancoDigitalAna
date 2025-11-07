49418fdf567a90f09d8e27a87ea32839720435bd
Davi <davi.frrs@outlook.com>
2025-11-07 09:04:58 -0300

    tests: CPF unit tests

---
+++ BancoDigitalAna.UnitTests/ContaCorrente/CpfTests.cs
@@ -0,0 +1,54 @@
+﻿using BancoDigitalAna.BuildingBlocks.Domain.Exceptions;
+using BancoDigitalAna.ContaCorrente.Domain.ValueObjects;
+using FluentAssertions;
+
+namespace BancoDigitalAna.UnitTests.ContaCorrente
+{
+
+    public class CpfTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData(" ")]
+        public void CpfVazio_EmBranco_ComEspaco_DeveLancarExcecao(string cpfTexto)
+        {
+            // Act
+            var act = () => Cpf.Criar(cpfTexto);
+
+            // Assert
+            act.Should().Throw<DomainException>()
+                .WithMessage("O campo CPF não pode ser vazio")
+                .Where(e => e.ErrorCode == "INVALID_DOCUMENT") ;
+        }
+
+        [Theory]
+        [InlineData("111.111.111-11")]
+        [InlineData("000.000.000-00")]
+        [InlineData("789.456.123-10")]
+        public void CpfInvlaido_DeveLancarExcecao (string cpfTexto)
+        {
+            // Act
+            var act = () => Cpf.Criar(cpfTexto);
+
+            // Assert
+            act.Should().Throw<DomainException>()
+                .WithMessage("CPF Inválido")
+                .Where(e => e.ErrorCode == "INVALID_DOCUMENT");
+        }
+
+        [Theory]
+        [InlineData("123.456.789-09")]
+        [InlineData("12345678909")]
+        public void CpfValido_DeveRetornarSucesso(string  cpfTexto)
+        {
+            // Act
+            var cpf = Cpf.Criar(cpfTexto);
+
+            // Assert
+            cpf.Should().NotBeNull();
+            cpf.Numero.Should().Be("12345678909");
+        }
+
+    }
+}
---
+++ BancoDigitalAna.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BancoDigitalAna.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
