f043c8de22a4814c8671d6d4a3112476b606ccaa
Davi <davi.frrs@outlook.com>
2025-11-07 10:27:42 -0300

    test: senha deve ter 6 caracteres

--- BancoDigitalAna.UnitTests/ContaCorrente/SenhaTests.cs
+++ BancoDigitalAna.UnitTests/ContaCorrente/SenhaTests.cs
@@ -15,6 +15,20 @@ namespace BancoDigitalAna.UnitTests.ContaCorrente
             // Act
             var act = () => Senha.Criar(senha);
 
+            // Assert
+            act.Should().Throw<DomainException>()
+                .WithMessage("Senha deve ter no mínimo 6 caractéres")
+                .Where(e => e.ErrorCode == "INVALID_PASSWORD");
+        }
+
+        [Theory]
+        [InlineData("senh4")]
+        public void SenhaInvalida_MenosDe6Caracteres_DeveLancarExcecao(string senha)
+        {
+            // Act
+            var act = () => Senha.Criar(senha);
+
+            // Assert
             act.Should().Throw<DomainException>()
                 .WithMessage("Senha deve ter no mínimo 6 caractéres")
                 .Where(e => e.ErrorCode == "INVALID_PASSWORD");
