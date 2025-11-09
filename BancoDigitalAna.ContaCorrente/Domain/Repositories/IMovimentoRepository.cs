56cd9055d166efeec1e84066708c52a3d3ee9daa
Davi <davi.frrs@outlook.com>
2025-11-09 20:09:33 -0300

    refactor: remoção repositories desnecessarios

--- BancoDigitalAna.ContaCorrente/Domain/Repositories/IMovimentoRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BancoDigitalAna.Conta.Domain.Entities;
-
-namespace BancoDigitalAna.Conta.Domain.Repositories
-{
-    public interface IMovimentoRepository
-    {
-        Task novoMovimento(Movimento request);
-    }
-}
